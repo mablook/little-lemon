3c158939cd096e1e91bc606b70ce63f297d1bf1e
Marcelo Bossle <mablook@gmail.com>
2024-04-28 10:33:57 +0100

    update layout
    Signed-off-by: Marcelo Bossle <mablook@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Container } from './components/templates/Container/Container';
 
 function App() {
   return (
     <div className="App">
-      Homepage
+      <Container>
+        Homepage
+      </Container>
     </div>
   );
 }
